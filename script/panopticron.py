b1c6deb77de535c55f3644ed227ba5529a6d3581
Adnene Guessoum <adnen.guessoum@gmail.com>
2024-12-21 16:34:25 +0100

    add: request to GITHUB API

--- script/panopticron.py
+++ script/panopticron.py
@@ -1,4 +1,50 @@
 """ Script to check on specific gh user's contributions, and send an email. """
+import logging
+import os
+
+import requests
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+load_dotenv()
+PERSONAL_GITHUB_USERNAME = os.getenv("PERSONAL_GITHUB_USERNAME")
+PERSONAL_GITHUB_TOKEN = os.getenv("PERSONAL_GITHUB_TOKEN")
+
+
+def get_user_activity(username):
+    """request target user's activity from GitHub API based on username."""
+
+    url = f"https://api.github.com/users/{username}/events"
+    headers = {
+        "Authorization": f"token {os.getenv('PERSONAL_GITHUB_TOKEN')}",
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+        response.raise_for_status()
+    except requests.exceptions.Timeout as err:
+        retry = 0
+        while retry < 3:
+            logger.warning("Timeout on %s, retrying... %s/3", url, retry)
+            response = requests.get(url, timeout=5)
+            if response.status_code == 200:
+                break
+            retry += 1
+        raise SystemExit(err) from err
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err) from err
+
+    if not bool(response.json()):  # empty response
+        logger.error("No data found for %s", username)
+        logger.info(
+            "Check if the username is correct. %s"
+            "Otherwise, check the GitHub API documentation for more information : "
+            "https://docs.github.com/rest",
+            username,
+        )
+        return None
+
+    return response.json()
 
 
 def main():
--- tests/test_panopticron.py
+++ tests/test_panopticron.py
@@ -1,5 +1,13 @@
 """test file for panopticron.py script"""
+import os
+
+from dotenv import load_dotenv
 from script import panopticron
+
+load_dotenv()
+
+# use the following to test with own GitHub username
+PERSONAL_GITHUB_USERNAME = os.getenv("PERSONAL_GITHUB_USERNAME")
 
 
 class TestPanopticron:
@@ -9,3 +17,18 @@ class TestPanopticron:
         assert capture.out == "TODO\n"
         assert capture.err == ""
         assert return_value == 0
+
+    def test_working_get_user_activity(self):
+        response = panopticron.get_user_activity(PERSONAL_GITHUB_USERNAME)
+        assert response is not None
+        assert isinstance(response, list)
+
+    def test_get_user_activity_wrong_username(self, capsys, caplog):
+        username = "nonexistentuser"
+        response = panopticron.get_user_activity(username)
+        capture_out_err = capsys.readouterr()
+        capture_log = caplog.text
+        assert capture_out_err.out == ""
+        assert capture_out_err.err == ""
+        assert f"No data found for {username}" in capture_log
+        assert response is None
